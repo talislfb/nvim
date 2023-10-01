d8cf4bbae37c9263f3821c79046e5cd66047f05f
Talis Barbalho <talis.lfb@gmail.com>
2023-09-30 20:32:49 -0400

    + clangd lsp

--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -1,3 +1,24 @@
+-- https://clangd.llvm.org/extensions.html#switch-between-sourceheader
+local function switch_source_header(bufnr)
+	bufnr = util.validate_bufnr(bufnr)
+	local clangd_client = util.get_active_client_by_name(bufnr, "clangd")
+	local params = { uri = vim.uri_from_bufnr(bufnr) }
+	if clangd_client then
+		clangd_client.request("textDocument/switchSourceHeader", params, function(err, result)
+			if err then
+				error(tostring(err))
+			end
+			if not result then
+				print("Corresponding file cannot be determined")
+				return
+			end
+			vim.api.nvim_command("edit " .. vim.uri_to_fname(result))
+		end, bufnr)
+	else
+		print("method textDocument/switchSourceHeader is not supported by any servers active on the current buffer")
+	end
+end
+
 return {
     "neovim/nvim-lspconfig",
     event = { "BufReadPre", "BufNewFile" },
@@ -63,6 +84,19 @@ return {
         lspconfig["pyright"].setup({
             capabilities = capabilities,
             on_attach = on_attach,
+        })
+
+        lspconfig["clangd"].setup({
+            capabilities = capabilities,
+            on_attach = on_attach,
+            commands = {
+                ClangdSwitchSourceHeader = {
+                    function()
+                        switch_source_header(0)
+                    end,
+                    description = "Switch between source/header",
+                },
+            },
         })
 
         -- configure lua server (with special settings)
